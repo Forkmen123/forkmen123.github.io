cbcddbc63bc9a74a3a965ad591f3e2db7df8b067
Forkmen123 <154713224+Forkmen123@users.noreply.github.com>
2024-12-28 16:41:20 -0500

    before-nextjs

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,4 +1,4 @@
-import Button from './components/Button'
+import Button from './components/Button';
 
 function App() {
   const texte_accueil = "Voici mon site internet, où je partage principalement mes peintures à l'huile. Vous y découvrirez une variété de sujets, allant des natures mortes aux paysages en passant par les portraits.";
@@ -6,15 +6,48 @@ function App() {
 
   return (
     <>
-      <div className="container m-0">
+      <div className="container-fluid">
+        {/* left part */}
         <div className="row" style={{ height: "100vh" }}>
-          <div className="left-part col-6 p-5">
+          <div className="left-part col-5 p-5">
             <h1>{name}</h1>
-            <Button onClick={() => console.log("clicked")}>Contacter</Button>
+            <div className="float-end mt-5">
+              <Button onClick={() => console.log("clicked")}>Contacter</Button>
+            </div>
           </div>
-          <div className="col-6 p-5">
-            <div>{texte_accueil}</div>
+          {/* right part */}
+          <div className="col p-5 my-5">
+            <nav>
+              <ul className="nav nav-pills">
+                <li className="nav-item">
+                    <Button>Projets</Button>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    Contact
+                  </a>
+                </li>
+                <li className="nav-item">
+                  <a className="nav-link" href="#">
+                    À propos
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <p style={{ width: "25rem" }}>{texte_accueil}</p>
+            <img
+              src="/images/photos-de-moi/moi-jaune.jpg"
+              alt="Photo Tommy Roy"
+              className="img-fluid w-25 rounded shadow float-end mt-5"
+              style={{ rotate: "5deg" }}
+            />
           </div>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <h2>Projets</h2>
         </div>
       </div>
     </>
