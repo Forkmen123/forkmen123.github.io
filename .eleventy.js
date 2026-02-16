de74aeb092c8a296e98fa1648625065d6477c1e6
Tommy Roy <tommyroy1432@gmail.com>
2026-02-15 23:19:32 -0500

    css fonctionne

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,9 @@ module.exports = function(eleventyConfig) {
   // copy the css / images directly to the output
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/lightbox2-2.11.4");
 
   return {
     dir: {
