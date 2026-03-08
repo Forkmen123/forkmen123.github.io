9778f3725039bf60b2113b9a85bd2189c433c917
Tommy Roy <tommyroy1432@gmail.com>
2026-03-08 15:27:06 -0400

    fichiers manquants

--- .eleventy.js
+++ .eleventy.js
@@ -1,78 +1,89 @@
 const markdownIt = require("markdown-it");
+const markdownItAnchor = require("markdown-it-anchor");
 const katex = require("@iktakahiro/markdown-it-katex");
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 
-module.exports = function(eleventyConfig) {
-// ------------ LaTeX ---------- 
+module.exports = function (eleventyConfig) {
+  // Fonction pour créer des slugs français
+  function slugify(s) {
+    return s
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/^-+|-+$/g, "");
+  }
+
+  // ------------ Markdown avec LaTeX ET anchors ----------
   const mdLib = markdownIt({
     html: true,
     breaks: true,
-    linkify: true
-  }).use(katex);
+    linkify: true,
+  })
+    .use(katex)
+    .use(markdownItAnchor, {
+      slugify: slugify,
+      permalink: false,
+    });
 
   eleventyConfig.setLibrary("md", mdLib);
 
-// ------------ table of contents ------------------
-
-  eleventyConfig.addFilter('getTOC', function(content) {
+  // ------------ table of contents ------------------
+  eleventyConfig.addFilter("getTOC", function (content) {
     const $ = cheerio.load(content);
-    
-    let toc = '<ul class="toc-list">';
-    
-    $('h1, h2, h3').each(function() {
+    let toc = '<p>';
+
+    $("h1, h2, h3").each(function () {
       const text = $(this).text();
-      let id = $(this).attr('id');
-      
-      // Create ID if it doesn't exist
-      if (!id) {
-        id = text.toLowerCase().replace(/[^a-z0-9]+/g, '-');
-        $(this).attr('id', id);
+      const id = $(this).attr("id");
+
+      if (id) {
+        const level = this.name;
+        toc += `<p class="${level}"><a href="#${id}">${text}</a></p>`;
       }
-      
-      const level = this.name; // h1, h2, or h3
-      toc += `<li class="${level}"><a href="#${id}">${text}</a></li>`;
     });
-    
-    toc += '</ul>';
+
+    toc += "</p>";
     return toc;
   });
 
-
-
-// ------------ ajout de collections --------
-  eleventyConfig.addCollection("recentPosts", function(collectionApi) {
-    return collectionApi.getFilteredByGlob("src/blog/*.md").reverse().slice(0, 3);
+  // ------------ ajout de collections --------
+  eleventyConfig.addCollection("recentPosts", function (collectionApi) {
+    return collectionApi
+      .getFilteredByGlob("src/blog/*.md")
+      .reverse()
+      .slice(0, 3);
   });
-  
-  eleventyConfig.addCollection("blogPosts", function(collectionApi) {
-      return collectionApi.getFilteredByGlob("src/blog/*.md").reverse();
+
+  eleventyConfig.addCollection("blogPosts", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("src/blog/*.md").reverse();
   });
-  
-// ----------------- ajout de filtres ---------------- 
+
+  // ----------------- ajout de filtres ----------------
   eleventyConfig.addFilter("readableDate", (dateObj) => {
-    return dateObj.toLocaleDateString('fr-CA', { 
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric'
+    return dateObj.toLocaleDateString("fr-CA", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
     });
   });
 
-// --------------- Passthrough copies -------------
+  // --------------- Passthrough copies -------------
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/images");
   eleventyConfig.addPassthroughCopy("src/assets");
   eleventyConfig.addPassthroughCopy("src/script");
   eleventyConfig.addPassthroughCopy("src/lightbox2-2.11.4");
 
-// ---------------- ignore files ----------------
+  // ---------------- ignore files ----------------
   eleventyConfig.ignores.add("_site");
-  eleventyConfig.ignores.add("src/_includes/_site/"); 
+  eleventyConfig.ignores.add("src/_includes/_site/");
 
   return {
     dir: {
       input: "src",
       includes: "_includes",
-      output: "_site"
-    }
+      output: "_site",
+    },
   };
 };
